008083eb0557bb8a409a6e0674afce74e0a2160d
Chris <christopher-dev@hoelter.email>
2020-06-21 13:32:28 -0400

    finalized message sending

--- GlobalEntryTimerTrigger/__init__.py
+++ GlobalEntryTimerTrigger/__init__.py
@@ -1,14 +1,43 @@
 import datetime
 import logging
-
+import os
 import azure.functions as func
+import requests
+from twilio.rest import Client
 
+# LOCATION_CODE = '6840' # Minneapolis
+LOCATION_CODE = '7740' # Milwaukee
+GLOBAL_ENTRY_URL = f'https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=3&locationId={LOCATION_CODE}&minimum=1'
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+    try:
+        data = requests.get(GLOBAL_ENTRY_URL).json()
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
+        if not data:
+            logging.warning('No response found')
+            return
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+        dates = []
+        for o in data:
+            if o['active']:
+                dt = o['startTimestamp']
+                dtp = datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M')
+                dates.append(dtp.strftime('%A, %B %d @ %I:%M%p'))
+
+        if not dates:
+            logging.info('No active dates found')
+            return
+
+        message = f'{len(dates)} new Milwaukee global entry appointment available, the earliest on {dates[0]}'
+
+        logging.info(f'Sending message: {message}')
+
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token)
+        sentMessage = client.messages.create(body=message, to=os.environ['TARGET_NUMBER'], from_=os.environ['TWILIO_NUMBER'])
+
+        logging.info(f'Message sent: {sentMessage}')
+
+    except:
+        logging.error('Something went wrong.')
